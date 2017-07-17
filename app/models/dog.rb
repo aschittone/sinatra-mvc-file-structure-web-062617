7a2230234cdd19f76f9427f4c0073c3ed34ae920
github username <github email address>
2017-07-17 14:08:27 -0400

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,16 @@
-class Model
-
+class Dog
   attr_accessor :name, :breed, :age
+
+  @@dogs = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@dogs << self
   end
 
+  def self.all
+    @@dogs
+  end
 end
